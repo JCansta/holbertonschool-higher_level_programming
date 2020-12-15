4715cbad8d9516d47dd630d99a785aeb5f3fcfe0
Jaime Castro <jaime.castro03@icloud.com>
2020-12-15 16:00:38 -0500

    changing

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,15 +1,28 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * check_palindrome - check if palindrome
- * @temp: the head of the list
- * @count: the len of the list
- * Return: 1 or 0
+ * is_palindrome - check if linked list is palindrome
+ * @head: the head of the list
+ * Return: 1 if yes 0 if not
  */
-int check_palindrome(listint_t *temp, int count)
+int is_palindrome(listint_t **head)
 {
+	listint_t *temp = *head;
+	int count = 0, end, ch;
+
+	if (temp == NULL)
+		return (1);
+	while (temp)
+	{
+		count++;
+		temp = temp->next;
+	}
+	temp = *head;
+	/*
+	 * check = check_palindrome(temp, count);
+	 */
+	end = count - 1;
 	int list[count];
-	int ch = 0, end = count - 1;
 
 	while (temp)
 	{
@@ -26,25 +39,4 @@ int check_palindrome(listint_t *temp, int count)
 		end--;
 	}
 	return (1);
-}
-/**
- * is_palindrome - check if linked list is palindrome
- * @head: the head of the list
- * Return: 1 if yes 0 if not
- */
-int is_palindrome(listint_t **head)
-{
-	listint_t *temp = *head;
-	int count = 0, check;
-
-	if (temp == NULL)
-		return (1);
-	while (temp)
-	{
-		count++;
-		temp = temp->next;
-	}
-	temp = *head;
-	check = check_palindrome(temp, count);
-	return (check);
 }
